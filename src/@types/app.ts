94799612829dc264d45727949629d1a8f45253c1
joaobuga35 <joaobuga35@gmail.com>
2023-01-24 20:47:18 -0300

    First server running

--- src/@types/app.ts
+++ src/@types/app.ts
@@ -3,6 +3,11 @@ import express, { Application, json, Request, Response } from "express";
 const app: Application = express();
 app.use(json());
 
+app.post("/login", (request:Request, response:Response) => {
+    console.log(request.body)
+    return response.status(201).json({message:"Usuário criado"})
+})
+
 const PORT:number = 3000
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
