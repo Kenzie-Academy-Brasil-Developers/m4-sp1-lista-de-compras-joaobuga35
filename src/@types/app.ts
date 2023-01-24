b962570f49834be92da32178e11ef80ab2f2afb0
joaobuga35 <joaobuga35@gmail.com>
2023-01-24 18:14:04 -0300

    initial commit

---
+++ src/@types/app.ts
@@ -0,0 +1,10 @@
+import express, { Application, json, Request, Response } from "express";
+
+const app: Application = express();
+app.use(json());
+
+const PORT:number = 3000
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
+
