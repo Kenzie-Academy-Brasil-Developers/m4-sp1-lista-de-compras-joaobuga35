d736a7a5520229e9217f8821f2d9a6cf25e9ea0d
joaobuga35 <joaobuga35@gmail.com>
2023-01-26 14:48:46 -0300

    get itens with id its ok

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application, json, Request, Response } from "express";
-import { createList } from "./logic";
+import { createList,getList,getListWithId } from "./logic";
 
 const app: Application = express();
 app.use(json());
 
 app.post("/purchaseList", createList)
+app.get("/purchaseList", getList)
+app.get("/purchaseList/:id",getListWithId)
 
 const PORT:number = 3000
 app.listen(PORT, () => {
--- src/logic.ts
+++ src/logic.ts
@@ -4,14 +4,28 @@ import { dataBase,dataBaseIds } from "./database";
 
 export const createList = (request: Request, response:Response) : Response => {
     const body:iList = request.body
-    
+
     const id = dataBase.length + 1
     const newBody:idType = {
-        id:id,
-        ...body
+        ...body,
+        id:id
     }
 
     dataBaseIds.push(id)
     dataBase.push(newBody)
     return response.status(201).json({newBody})
+}
+
+export const getList = (request: Request, response: Response): Response => {
+    console.log(request.body)
+
+    return response.status(200).json({ dataBase })
+}
+
+export const getListWithId = (request: Request, response: Response): Response => {
+    const idList: number = parseInt(request.params.id)
+
+    const indexAboutListId = dataBase.findIndex((elem) => elem.id === idList)
+
+    return response.status(200).json(dataBase[indexAboutListId])
 }
